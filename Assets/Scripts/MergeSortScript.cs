1b5f56734736aaa2d1a370173c38212c0bda7a81
Tuukka Muukkonen <tuukka.o.muukkonen@gmail.com>
2016-05-27 01:58:27 +0300

    MergeSort working

--- Assets/Scripts/MergeSortScript.cs
+++ Assets/Scripts/MergeSortScript.cs
@@ -7,66 +7,40 @@ public class MergeSortScript : MonoBehaviour {
 
 	public Text unsorted;
 	public Text sorted;
+	public Text data;
 
 	public int listSize;
-	public int maxValue;
 
-	public List<int> randomList;
+	List<int> randomList;
+	List<int> sortedList;
+
+	float highestDeltaTime;
 
 	void Start () {
-		randomList = new List<int>();
-		for (int i = 0; i < listSize; i++) {
-			int randomInt = Random.Range(0, maxValue);
-			randomList.Add(randomInt);
-		}
+		RandomizeList();
 		unsorted.text = ListToString(randomList);
 	}
 
 	void Update () {
-
-		if (Input.GetKeyDown(KeyCode.R)) {
-			//sorted.text = ListToString(MergeSort(randomList));
-			MergeSort(randomList);
-		}
-
-		if (Input.GetKeyDown(KeyCode.W)) {
-			List<int> l1 = new List<int>();
-			List<int> l2 = new List<int>();
-
-			// Divides the list a into 2 lists
-			for (int i = 0; i < randomList.Count; i++) {
-				if (i <= randomList.Count / 2)
-					l1.Add(randomList[i]);
-				else
-					l2.Add(randomList[i]);
-			}
-
-			for (int i = 0; i < l1.Count; i++) {
-				print("" + l1[i]);
-			}
-			for (int i = 0; i < l2.Count; i++) {
-				print("" + l2[i]);
-			}
-		}
+		UpdateData();
+		UserInputs();
 	}
 
 	List<int> MergeSort (List<int> a) {
-		//print(a);
-		if (a.Count <= 1)
+		if (a.Count == 1) {
 			return a;
+		}
 
 		List<int> l1 = new List<int>();
 		List<int> l2 = new List<int>();
 
-		// Divides the list a into 2 lists
 		for (int i = 0; i < a.Count; i++) {
-			if (i <= a.Count / 2)
+			if (i < a.Count / 2) {
 				l1.Add(a[i]);
-			else
+			} else {
 				l2.Add(a[i]);
+			}
 		}
-
-		//print(l1 + "" + l2);
 
 		l1 = MergeSort(l1);
 		l2 = MergeSort(l2);
@@ -78,8 +52,7 @@ public class MergeSortScript : MonoBehaviour {
 		List<int> c = new List<int>();
 
 		while (a.Count > 0 && b.Count > 0) {
-			//print("While 1");
-			if (a[0] > b[0]) {
+			if (a[0] < b[0]) {
 				c.Add(a[0]);
 				a.Remove(a[0]);
 			} else {
@@ -89,13 +62,11 @@ public class MergeSortScript : MonoBehaviour {
 		}
 
 		while (a.Count > 0) {
-			//print("While 2");
 			c.Add(a[0]);
 			a.Remove(a[0]);
 		}
 
 		while (b.Count > 0) {
-			//print("While 3");
 			c.Add(b[0]);
 			b.Remove(b[0]);
 		}
@@ -105,9 +76,46 @@ public class MergeSortScript : MonoBehaviour {
 
 	string ListToString (List<int> l) {
 		string s = "";
-		for (int i = 0; i < l.Count; i++) {
+		for (int i = 0; i <= 400; i++) {
 			s += l[i] + " ";
 		}
 		return s;
+	}
+
+	void RandomizeList () {
+		randomList = new List<int>();
+		for (int i = 0; i < listSize; i++) {
+			int randomInt = Random.Range(0, listSize);
+			randomList.Add(randomInt);
+		}
+	}
+
+	void UpdateData () {
+		if (Time.deltaTime > highestDeltaTime) {
+			highestDeltaTime = Time.deltaTime;
+		}
+		data.text = Mathf.Round(Time.deltaTime * 1000) + " ms - Highest " + Mathf.Round(highestDeltaTime * 1000) + " ms";
+	}
+
+	void UserInputs () {
+		if (Input.GetKeyDown(KeyCode.R)) {
+			RandomizeList();
+		}
+
+		if (Input.GetKeyDown(KeyCode.E)) {
+			sortedList = MergeSort(randomList);
+		}
+
+		if (Input.GetKeyDown(KeyCode.W)) {
+			sorted.text = ListToString(sortedList);
+			unsorted.text = ListToString(randomList);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			RandomizeList();
+			sortedList = MergeSort(randomList);
+			sorted.text = ListToString(sortedList);
+			unsorted.text = ListToString(randomList);
+		}
 	}
 }
