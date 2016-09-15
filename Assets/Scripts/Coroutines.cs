61fd9730be0e2abb1945ba954a03a1c76a63ec4e
Tuuga-PC\Tuuga <tuukka.o.muukkonen@gmail.com>
2016-09-15 18:35:56 +0300

    Coroutines and MyUpdate

---
+++ Assets/Scripts/Coroutines.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coroutines : MonoBehaviour {
+
+	void Start () {
+		StartCoroutine(MyUpdate());
+	}
+
+	IEnumerator MyUpdate () {
+		while (true) {
+
+			yield return new WaitForEndOfFrame();
+		}
+	}
+}
