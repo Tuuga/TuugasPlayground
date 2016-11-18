56b751b63255ce30fe6944adff5d4a4b1a9b0bb2
Tuuga-PC\Tuuga <tuukka.o.muukkonen@gmail.com>
2016-11-18 23:37:10 +0200

    OneLinePerFunction movement script

---
+++ Assets/OneLinePerFunction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneLinePerFunction : MonoBehaviour {
+	
+	void Update () { Move(); }
+	void Move () { transform.position += new Vector3(Left() + Right(), 0, Forward() + Back()) * Time.deltaTime * MovementSpeed(); }
+
+	float MovementSpeed() { return 10f; }
+
+	float Left		() { return Input.GetKey(KeyCode.A) ? -1 : 0; }
+	float Right		() { return Input.GetKey(KeyCode.D) ? 1 : 0; }
+	float Forward	() { return Input.GetKey(KeyCode.W) ? 1 : 0; }
+	float Back		() { return Input.GetKey(KeyCode.S) ? -1 : 0; }
+}
